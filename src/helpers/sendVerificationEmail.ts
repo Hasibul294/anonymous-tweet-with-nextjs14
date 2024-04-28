e612f865e589ad1ae991fb4e60a2ae376942fe80
Hasibul294 <hasibulhasan294@gmail.com>
2024-04-28 18:00:33 +0600

    delete some unwanted file

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -4,10 +4,17 @@ import { ApiResponse } from "@/types/apiResponse";
 
 export async function sendVerificationEmail(
   email: string,
-  userName: string,
+  username: string,
   verifyCode: string
 ): Promise<ApiResponse> {
   try {
+    await resend.emails.send({
+      from: "<onboarding@resend.dev>",
+      to: email,
+      subject: "Anonymous Tweet | Verification code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+
     return {
       success: true,
       message: "verification email send successfully",
